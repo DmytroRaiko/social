73a7ed9c93f30027933a8e50261abdf6d0d6ed5a
DmytroRaiko <55841333+DmytroRaiko@users.noreply.github.com>
2021-12-30 08:59:13 +0200

    react init

--- api/src/app.js
+++ api/src/app.js
@@ -1,4 +1,30 @@
-// eslint-disable-next-line no-console
-console.log('hw');
-// eslint-disable-next-line no-console
-console.log(process.env.APP_PORT);
+const express = require('express');
+
+const app = express();
+const { Pool } = require('pg');
+const config = require('./services/config');
+
+const port = config.appPort;
+
+const pool = new Pool({
+  user: config.dbUser,
+  host: config.dbHost,
+  database: config.db,
+  password: config.dbPass,
+  port: config.dbPort,
+});
+
+app.get('/', (req, res) => {
+  pool.query('SELECT profileid, email, name FROM profile', (error, results) => {
+    if (error) {
+      throw error;
+    } else {
+      res.status(200).json(results.rows);
+    }
+  });
+});
+
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`App listening at http://localhost:${port}`)
+});
---
+++ api/src/services/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  appPort: process.env.APP_PORT,
+  dbHost: process.env.DB_HOST,
+  dbPort: process.env.DB_PORT,
+  dbPass: process.env.DB_PASS,
+  dbUser: process.env.DB_USER,
+  db: process.env.DATABASE,
+};
