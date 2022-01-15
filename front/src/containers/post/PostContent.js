5b1e91b5fd9aa0c142cde98296452ed0ed250e38
DmytroRaiko <55841333+DmytroRaiko@users.noreply.github.com>
2022-01-16 00:00:03 +0200

    Front props. Code refactoring.

--- front/src/containers/post/PostContent.js
+++ front/src/containers/post/PostContent.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import PostText from '../../components/post/PostText';
 import PostImg from '../../components/post/PostImg';
+import postImgProps from '../../PropTypes/PostImgProps';
 import postImgPropsDefault from '../../PropTypes/PostImgPropsDefault';
 
 const PostContent = ({ postText, postImg }) => (
@@ -12,18 +13,11 @@ const PostContent = ({ postText, postImg }) => (
 
 PostContent.propTypes = {
   postText: PropTypes.string.isRequired,
-  postImg: PropTypes.shape(
-    {
-      src: PropTypes.string,
-      title: PropTypes.string,
-    },
-  ),
+  postImg: PropTypes.shape(postImgProps),
 };
 
 PostContent.defaultProps = {
-  postImg: {
-    postImgPropsDefault,
-  },
+  postImg: postImgPropsDefault,
 };
 
 export default PostContent;
